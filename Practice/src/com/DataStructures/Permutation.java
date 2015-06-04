e24698331a0624ad72f09b492be2147bccb68123
Tovar-Erick <sephiroth_eri@hotmail.com>
2015-06-04 00:05:08 -0500

    Permutation code completed

--- Practice/src/com/DataStructures/Permutation.java
+++ Practice/src/com/DataStructures/Permutation.java
@@ -6,9 +6,49 @@ package com.DataStructures;
 import java.util.HashMap;
 
 /**
- * @author TOSHIBA
+ * @author Tovar
  *
  */
+
+//Verify if a String is a permutation of another String
 public class Permutation {
-	HashMap map = new HashMap();
+	private HashMap<Character, Integer> map;
+	
+	public Permutation(){
+		map = new HashMap<Character, Integer>();
+	}
+	
+	private void addString2Map(String p_cadena){
+		for(char cha: p_cadena.toCharArray()){
+			if(!map.containsKey(cha))
+				map.put(cha, 1);
+			else
+				map.put(cha, map.get(cha)+1);
+		}
+	}
+	
+	private void removeString2Map(String p_cadena){
+		for(char cha: p_cadena.toCharArray()){
+			if(map.containsKey(cha)){
+				if(map.get(cha)>1)
+					map.put(cha, map.get(cha)-1);
+				else
+					map.remove(cha);
+			}
+		}
+	}
+	
+	private boolean isMapEmpty(){
+		return map.isEmpty();
+	}
+	
+	public static void main(String[] args){
+		Permutation permut = new Permutation();
+		permut.addString2Map("Erick");
+		permut.removeString2Map("kirEc");
+		if(permut.isMapEmpty())
+			System.out.println("Im a permutation!");
+		else
+			System.out.println("Im not a prmutation...");
+	}
 }
